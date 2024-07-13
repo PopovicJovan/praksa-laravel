3de62c82d4b09618e91d832a909c939c9cd1d8e9
PopovicJovan <jovanp.school06@gmail.com>
2024-07-13 09:46:16 +0200

    Movie and Genre resources are created

---
+++ app/Http/Resources/MovieResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MovieResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'adult' => $this->adult,
+            'title' => $this->title,
+            'overview' => $this->overview,
+            'popularity' => $this->popularity,
+            'vote_average' => $this->vote_average,
+            'vote_count' => $this->vote_count,
+            'release_date' => $this->release_date,
+            'genres' => array_map(
+                function ($a) {return $a->id;},
+                $this->genres->all()
+            )
+        ];
+
+
+    }
+}
